93aa48cf183d700034b89c05f62164c596855c7f
Florin Lazar <florin.lazar.it2014@gmail.com>
2021-11-19 09:23:46 +0200

    Iterate and Print array

--- ArrayExercises/ArrayExercises/Program.cs
+++ ArrayExercises/ArrayExercises/Program.cs
@@ -8,5 +8,25 @@ namespace ArrayExercises
         {
             Console.WriteLine("Hello World!");
         }
+
+        static void PrintArray(int[] array)
+        {
+            if (array is null)
+            {
+                Console.WriteLine("Array is null");
+                return;
+            }
+
+            Console.Write("Array=");
+            for (int i = 0; i < array.Length; i++)
+            {
+                int element = array[i];
+                Console.Write(element);
+                if (i < array.Length - 1)
+                {
+                    Console.Write(",");
+                }
+            }
+        }
     }
 }
